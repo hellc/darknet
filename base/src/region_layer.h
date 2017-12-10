d79ba9959ea2ff0f47cf989ff645bfdc54aa0163
ivanmanov <ivanmanov@live.com>
2017-12-10 22:13:12 +0100

    vs solution
    cpp crl ready libdarknet_yolo library

--- base/src/region_layer.h
+++ base/src/region_layer.h
@@ -1,18 +1,20 @@
 #ifndef REGION_LAYER_H
 #define REGION_LAYER_H
 
-#include "darknet.h"
 #include "layer.h"
 #include "network.h"
 
-layer make_region_layer(int batch, int h, int w, int n, int classes, int coords);
-void forward_region_layer(const layer l, network net);
-void backward_region_layer(const layer l, network net);
+typedef layer region_layer;
+
+region_layer make_region_layer(int batch, int h, int w, int n, int classes, int coords);
+void forward_region_layer(const region_layer l, network_state state);
+void backward_region_layer(const region_layer l, network_state state);
+void get_region_boxes(layer l, int w, int h, float thresh, float **probs, box *boxes, int only_objectness, int *map);
 void resize_region_layer(layer *l, int w, int h);
 
 #ifdef GPU
-void forward_region_layer_gpu(const layer l, network net);
-void backward_region_layer_gpu(layer l, network net);
+void forward_region_layer_gpu(const region_layer l, network_state state);
+void backward_region_layer_gpu(region_layer l, network_state state);
 #endif
 
 #endif
